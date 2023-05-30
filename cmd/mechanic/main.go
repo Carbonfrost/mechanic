8d57eec01192b2fc0fce55b6e741fe99fbcbc0dd
Gary Mann <garythemann@users.noreply.github.com>
2023-05-30 12:05:35 -0700

    Relocate mechanic main into internal pkg

--- cmd/mechanic/main.go
+++ cmd/mechanic/main.go
@@ -1,60 +1,11 @@
 package main
 
 import (
-	"io"
 	"os"
 
-	"github.com/Carbonfrost/joe-cli"
-	"github.com/Carbonfrost/joe-cli/extensions/color"
-	"github.com/Carbonfrost/mechanic/internal/build"
-	"github.com/yuin/goldmark"
-	"github.com/yuin/goldmark/text"
+	"github.com/Carbonfrost/mechanic/internal/cmd/mechanic"
 )
 
 func main() {
-	createApp().Run(os.Args)
-}
-
-func createApp() *cli.App {
-	return &cli.App{
-		Name:     "mechanic",
-		HelpText: "",
-		Comment:  "Markdown processing and rendering",
-		Uses: cli.Pipeline(
-			&color.Options{},
-		),
-		Action: func(c *cli.Context) error {
-			return c.FileSet("files").Do(func(f *cli.File, err error) error {
-				if err != nil {
-					return err
-				}
-				return renderPage(f.Name)
-			})
-		},
-		Version: build.Version,
-		Args: []*cli.Arg{
-			{
-				Name:    "files",
-				Value:   new(cli.FileSet),
-				Options: cli.Merge,
-			},
-		},
-	}
-}
-
-func renderPage(file string) error {
-	f, err := os.Open(file)
-	if err != nil {
-		return err
-	}
-
-	source, err := io.ReadAll(f)
-	if err != nil {
-		return err
-	}
-
-	node := goldmark.DefaultParser().Parse(text.NewReader(source))
-	renderer := goldmark.DefaultRenderer()
-	renderer.Render(os.Stdout, source, node)
-	return nil
+	mechanic.Run(os.Args)
 }
---
+++ internal/cmd/mechanic/mechanic.go
@@ -0,0 +1,60 @@
+package mechanic
+
+import (
+	"io"
+	"os"
+
+	"github.com/Carbonfrost/joe-cli"
+	"github.com/Carbonfrost/joe-cli/extensions/color"
+	"github.com/Carbonfrost/mechanic/internal/build"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/text"
+)
+
+func Run(args []string) {
+	NewApp().Run(args)
+}
+
+func NewApp() *cli.App {
+	return &cli.App{
+		Name:     "mechanic",
+		HelpText: "",
+		Comment:  "Markdown processing and rendering",
+		Uses: cli.Pipeline(
+			&color.Options{},
+		),
+		Action: func(c *cli.Context) error {
+			return c.FileSet("files").Do(func(f *cli.File, err error) error {
+				if err != nil {
+					return err
+				}
+				return renderPage(f.Name)
+			})
+		},
+		Version: build.Version,
+		Args: []*cli.Arg{
+			{
+				Name:    "files",
+				Value:   new(cli.FileSet),
+				Options: cli.Merge,
+			},
+		},
+	}
+}
+
+func renderPage(file string) error {
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+
+	source, err := io.ReadAll(f)
+	if err != nil {
+		return err
+	}
+
+	node := goldmark.DefaultParser().Parse(text.NewReader(source))
+	renderer := goldmark.DefaultRenderer()
+	renderer.Render(os.Stdout, source, node)
+	return nil
+}
